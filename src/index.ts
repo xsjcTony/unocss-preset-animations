095fe37c95ba2ea2c564efba7da6a707a63bb44c
Tony Jiang <tjiang@nowfinance.com.au>
2023-11-20 17:18:24 +1100

    fix import type

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import { shortcuts } from './shortcuts'
 import { theme } from './theme'
 
 
+// This is for fixing the `rollup-plugin-dts` generating wrong import issue.
+// eslint-disable-next-line ts/no-unused-vars
+import type { Theme } from '@unocss/preset-mini'
+
+
 export const presetAnimations = definePreset(() => ({
   name: 'unocss-preset-animations',
   theme,
